e4acecd5840eb8db3f05739fa8f135d5a8e4897e
Michael Totaro <mtotaro@ncsu.edu>
2023-09-17 17:39:37 -0400

    Adding files

---
+++ script.js
@@ -0,0 +1,34 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = [] 
+
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+
+    users.forEach(user => {
+        const isVisible = user.song.toLowerCase().includes(value);
+        user.element.classList.toggle("hide", !isVisible);
+    })
+});
+
+fetch("fav_covers.json")
+    .then(res => res.json())
+    .then(data => {
+        users = data.FavoriteCovers.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+
+
+            header.textContent = user.Song
+            body.textContent = user.Appearance
+
+            userCardContainer.append(card)
+
+            return {song: user.Song, element: card }
+        })
+        
+    });
