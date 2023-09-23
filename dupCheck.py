a55069a7b26fbf5029223ffb13f58c6cccabbf44
Michael Totaro <mtotaro@ncsu.edu>
2023-09-23 15:56:23 -0400

    Adding files

---
+++ dupCheck.py
@@ -0,0 +1,12 @@
+# import json 
+
+
+# with open("fav_covers.json", 'r') as file:
+#     fav_data = json.load(file)
+
+#     used_songs = [] 
+
+
+#     for song in fav_data['FavoriteCovers']:
+#         if song['Link'] == "":
+#             print(song["Title"])
