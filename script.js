a50b42e97517671e1768b474e2e9692a78c9c1f9
Michael Totaro <mtotaro@ncsu.edu>
2023-11-05 15:16:34 -0500

    Added media query

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const links = ["https://youtu.be/7Yc3utlluNc&t=7509" ,
 "https://youtu.be/Qh8m_ldN2zw&t=5956" , 
 "https://youtu.be/QLLYLiTUyDY?feature=share&t=5669"];
 
-instruments.forEach(element => {
-    console.log(element)
+const allLinks = links.map((link, i) => 
+    `<div style="margin: 10px 0;">• <a href="${link}" style= "font-size: 25px;text-decoration: none;">Livestream  ${i}</a></div>`
+).join('');
+
+document.getElementById('result-links').innerHTML = allLinks;
+
+
+window.addEventListener("resize", function () {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    document.getElementById("cool").innerText = "Inner Width: " + w + ", Inner Height: " + h;
 });
