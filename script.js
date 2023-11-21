e896f3497c865ac9ccb9bd32c3a405e720efcb57
Michael Totaro <mtotaro@ncsu.edu>
2023-11-20 21:59:01 -0500

    Testing SongInfo

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-window.addEventListener("resize", function () {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
+window.addEventListener('load', () => {
 
-    document.getElementById("cool").innerText = "Width: " + w + ", Height: " + h;
+    const title = "Star Spangled Banner/American National Anthem (Jimi Hendrix Style)";
+    const artist = "Francis Scott Key";
+    const other_artists = "N/A";
+    const appears = "Livestream 50,Livestream 62";
+    const instruments = "Acoustic Guitar,  Electric Guitar";
+    const image = "Jimi Hendrix.jpg";
+    const theLinks = "https://youtu.be/rHhhcP4jGD8&t=5071 , https://youtu.be/1HkYscBWtp8?feature=share&t=2836";
+
+    theTitle = title.replace(" (Classical Guitar)", "").replace(" (Electric Song)", "");
+    theTitle += title.includes("Session #") ? " (Check comments for full timestamp)" : "";
+
+    document.getElementById('result-title').innerText = ": " + theTitle;
+    document.getElementById('result-artist').innerText = ": " + artist;
+    document.getElementById('result-otherart').innerText = ": " + (other_artists || "N/A");
+    document.getElementById('result-instruments').innerText = ": " + instruments;
+    
+    const mediaQuery = window.matchMedia('(min-device-width: 375px) and (max-device-width: 812px)');
+    
+    if (mediaQuery.matches) {
+        document.getElementById('result-instruments').innerHTML = "";
+
+        document.getElementById('result-instrument').innerHTML = instruments.split(",").map((instrument) => 
+        ` <div style="font-size:24px;margin: 6px 0; left: 5px;margin-left: 33px;"> • ${instrument} </div>`
+        ).join(''); 
+    }; 
+    
+    window.addEventListener('error', () => {
+        console.error("Image not found");
+        console.error("Image name: " + image);
+    }, true);
+
+    document.getElementById("pic1").src = image;
+
+    web = appears.split(",");
+
+    const alllinks = theLinks.split(" , ").map((link, i) => 
+    `<div style="margin: 10px 0;">• <a href="${link}" style= "font-size: 25px;text-decoration: none;">${web[i]}</a></div>`
+    ).join('');
+
+    document.getElementById('result-links').innerHTML = alllinks;
+
 });
