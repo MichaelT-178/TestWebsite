25eed1300054ddf06fe461b2d5336fd8bd0f4e83
Michael Totaro <mtotaro@ncsu.edu>
2024-01-03 22:41:17 -0500

    Made updates songinfo

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
 
-    const title = "Gold Moon";
+    const title = "Teal Moon";
     const artist = "Nick Drake";
     const other_artists = "";
     const appears = "Solo Video (Pink Moon Album/M),Livestream 74,Livestream 75 (DX1R),Livestream 81 (DX1R),Livestream 126 (DX1R),Livestream 164 (DX1R)";
-    const instruments = "Acoustic Guitar, (M) - Martin 00-15m, (DX1R) - Martin DX1R".replace(/,/g, "\t\t\t/\t\t\t");
+    const instruments = "Acoustic Guitar<span style='color: black; user-select: none;'>hhhhhhhhhhhh</span>"; // Modified this line" //, (M) - Martin 00-15m, (DX1R) - Martin DX1R".replace(/,/g, "\t\t\t/\t\t\t");
     const image = "Beck.jpg";
     const theLinks = "https://youtu.be/GtqrWBN9tsc&t=0 , https://youtu.be/WQ8mpI5z7z4&t=7275 , https://youtu.be/uLKs6770MzM&t=4465 , https://youtu.be/0vseNNLzOWY&t=8700 , https://youtu.be/ebv-JlkIPUY&t=1905 , https://youtu.be/c1H63fb1AJs?feature=share&t=5984";
 
@@ -14,17 +14,17 @@ window.addEventListener('load', () => {
     document.getElementById('result-title').innerText = ": " + theTitle;
     document.getElementById('result-artist').innerText = ": " + artist;
     document.getElementById('result-otherart').innerText = ": " + (other_artists || "N/A");
-    document.getElementById('result-instruments').innerText = ": " + instruments;
+    document.getElementById('result-instruments').innerHTML = ": " + instruments;
     
     const mediaQuery = window.matchMedia('(min-device-width: 375px) and (max-device-width: 812px)');
     
-    if (mediaQuery.matches) {
-        document.getElementById('result-instruments').innerHTML = "";
+    // if (mediaQuery.matches) {
+    //     document.getElementById('result-instruments').innerHTML = "";
 
-        document.getElementById('result-instrument').innerHTML = instruments.split(",").map((instrument) => 
-        ` <div style="font-size:24px;margin: 6px 0; left: 5px;margin-left: 33px;"> • ${instrument} </div>`
-        ).join(''); 
-    }; 
+    //     document.getElementById('result-instrument').innerHTML = instruments.split(",").map((instrument) => 
+    //     ` <div style="font-size:24px;margin: 6px 0; left: 5px;margin-left: 33px;"> • ${instrument} </div>`
+    //     ).join(''); 
+    // }; 
     
     window.addEventListener('error', () => {
         console.error("Image not found");
