aed1e6a7d1c0800e48c565abc3e2b8dbc180bf1f
Michael Totaro <mtotaro@ncsu.edu>
2024-01-04 20:54:10 -0500

    YEET

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 window.addEventListener('load', () => {
 
+
+    //If title, artist, other_artists, and instruments are all less than 27 
+    //add h's to instruments string to make it larger.
+
+
     const title = "Teal Moon";
     const artist = "Nick Drake";
     const other_artists = "";
     const appears = "Solo Video (Pink Moon Album/M),Livestream 74,Livestream 75 (DX1R),Livestream 81 (DX1R),Livestream 126 (DX1R),Livestream 164 (DX1R)";
-    const instruments = "Acoustic Guitar, (M) - Martin 00-15m, (DX1R) - Martin DX1R";  //"Acoustic Guitar<span style='color: black; user-select: none;'>hhhhhhhhhhhh</span>"; // Modified this line" //, (M) - Martin 00-15m, (DX1R) - Martin DX1R".replace(/,/g, "\t\t\t/\t\t\t");
+    let instruments = "Acoustic Guitar"; //, (M) - Martin 00-15m, (DX1R) - Martin DX1R";  //"Acoustic Guitar<span style='color: black; user-select: none;'>hhhhhhhhhhhh</span>"; // Modified this line" //, (M) - Martin 00-15m, (DX1R) - Martin DX1R".replace(/,/g, "\t\t\t/\t\t\t");
+    
+    //instruments = localstorage.getItem('instruments')
+
+
+    let numberOfH;
+    let hString;
+
+    //if condition is true add h's for length purposes to push thing to the left over so it's not dead center
+    //Kind of a ghetto fix but it works.
+    if (title.length < 28 && artist.length < 28 && other_artists.length < 28 && instruments.length < 28) {
+        numberOfH = 27 - instruments.length;
+        hString = 'h'.repeat(Math.max(numberOfH, 0));
+    }
+
+    
+    //WithHs user-select: none;
+    instruments = `${instruments}<span style='color: black; '>${hString}</span>`; 
+
     const image = "Beck.jpg";
     const theLinks = "https://youtu.be/GtqrWBN9tsc&t=0 , https://youtu.be/WQ8mpI5z7z4&t=7275 , https://youtu.be/uLKs6770MzM&t=4465 , https://youtu.be/0vseNNLzOWY&t=8700 , https://youtu.be/ebv-JlkIPUY&t=1905 , https://youtu.be/c1H63fb1AJs?feature=share&t=5984";
 
