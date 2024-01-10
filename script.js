32949dae7fe6a73097b9aaf6059365d23b39caef
Michael Totaro <mtotaro@ncsu.edu>
2024-01-10 18:24:35 -0500

    Testing on mobile

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 window.addEventListener('load', () => {
 
-    const title = "FULL Moon";
-    const artist = "Nick Drake";
+    const title = "A Thousand Years";
+    const artist = "Christina Perri";
     const other_artists = "";
-    const appears = "Solo Video (Pink Moon Album/M),Livestream 74,Livestream 75 (DX1R),Livestream 81 (DX1R),Livestream 126 (DX1R),Livestream 164 (DX1R)";
-    let instruments = "Acoustic Guitar, (M) - Martin 00-15m, (DX1R) - Martin DX1R"; 
-    
-    let numberOfH;
-    let hString;
-
-    //if condition is true add h's for length purposes to push thing to the left over so it's not dead center
-    //Kind of a ghetto fix but it works.
-    if (title.length < 28 && artist.length < 28 && other_artists.length < 28 && instruments.length < 28) {
-        numberOfH = 27 - instruments.length;
-        hString = 'h'.repeat(Math.max(numberOfH, 0));
-    }
-
-    instruments = `${instruments}<span style='color: red; user-select: none;'>${hString || ""}</span>`; 
-
-    const image = "Beck.jpg";
-    const theLinks = "https://youtu.be/GtqrWBN9tsc&t=0 , https://youtu.be/WQ8mpI5z7z4&t=7275 , https://youtu.be/uLKs6770MzM&t=4465 , https://youtu.be/0vseNNLzOWY&t=8700 , https://youtu.be/ebv-JlkIPUY&t=1905 , https://youtu.be/c1H63fb1AJs?feature=share&t=5984";
+    const appears = "Livestream 83";
+    const instruments = "Acoustic Guitar";
+    const image = `../pics/Christina Perri.jpg`;
+    const theLinks = "https://youtu.be/_7xQAOqGbk4&t=2792";
 
     theTitle = title.replace(" (Classical Guitar)", "").replace(" (Electric Song)", "");
     theTitle += title.includes("Session #") ? " (Check comments for full timestamp)" : "";
@@ -27,22 +14,16 @@ window.addEventListener('load', () => {
     document.getElementById('result-title').innerText = ": " + theTitle;
     document.getElementById('result-artist').innerText = ": " + artist;
     document.getElementById('result-otherart').innerText = ": " + (other_artists || "N/A");
-    document.getElementById('result-instruments').innerHTML = ": " + instruments;
+    document.getElementById('result-instruments').innerText = ": " + instruments;
     
     const mediaQuery = window.matchMedia('(min-device-width: 375px) and (max-device-width: 812px)');
-    const instrumentStyling = "font-size: 24px;" + 
-                              "color: lightBlue;" +
-                              "text-align: left;" +
-                              "margin-left: 12px;" +
-                              "margin-bottom: 9px;";
-
+    
     if (mediaQuery.matches) {
         document.getElementById('result-instruments').innerHTML = "";
-        
-        document.getElementById('result-instrument').innerHTML = instruments.split(",").map((instrument) => 
-        ` <div style="${instrumentStyling}"> • ${instrument} </div>`
-        ).join(''); 
 
+        document.getElementById('result-instrument').innerHTML = instruments.split(",").map((instrument) => 
+        ` <div style="font-size:24px;margin: 6px 0; left: 5px;margin-left: 33px;"> • ${instrument} </div>`
+        ).join(''); 
     }; 
     
     window.addEventListener('error', () => {
